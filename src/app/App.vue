b5320461a764f82e23ad1156652dd66995381bda
David Glassman <davidg@controlconcepts.net>
2024-01-11 16:42:09 -0500

    Work on ProjectTile component

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <div id="app" class="w-full h-screen bg-gray-50"></div>
+  <div id="app" class="w-full h-screen bg-gray-50 p-4 flex-col-standard justify-start">
+    <ProjectTile :image="'https://placehold.co/600x400'" :name="'Project Name'" :info="'Additional Project Info'" :content="'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et netus et malesuada fames ac turpis egestas integer. Auctor urna nunc id cursus metus aliquam eleifend mi. Sed turpis tincidunt id aliquet risus feugiat. Dui nunc mattis enim ut tellus elementum. Aliquet eget sit amet tellus cras adipiscing enim. Ultricies mi quis hendrerit dolor magna eget est. Quis vel eros donec ac odio tempor. Erat nam at lectus urna. Sed faucibus turpis in eu mi bibendum neque. Vel eros donec ac odio tempor orci dapibus ultrices in. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. A diam maecenas sed enim ut sem viverra aliquet eget.'" />
+  </div>
 </template>
 
 <script setup>
 // ------------------------ Imports
 
 // -------- Components
+
+import ProjectTile from "../components/ProjectTile.vue";
 
 // -------- Vue
 
---
+++ src/components/ProjectTile.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="w-auto h-auto">
+    <div class="tile w-full h-auto border border-gray-400 rounded-xl">
+      <img :src="image" alt="Some picture" class="image w-full border-b border-b-gray-400 object-cover m-0 p-0 rounded-t-xl" />
+
+      <div class="content w-full p-4 pb-0 relative">
+        <button class="w-10 h-10 rounded-full absolute top-0 right-0 m-4 flex-row-standard bg-green-400 text-white" :class="[{ 'opacity-0': show }, { 'opacity-100': !show }]" @click="show = !show">
+          <Icon :name="'plus'" :class="'w-6 h-6'" />
+        </button>
+
+        <button class="w-10 h-10 rounded-full absolute top-0 right-0 m-4 flex-row-standard bg-red-400 text-white" :class="[{ 'opacity-0': !show }, { 'opacity-100': show }]" @click="show = !show">
+          <Icon :name="'minus'" :class="'w-6 h-6'" />
+        </button>
+
+        <div class="header mb-4">
+          <h3 class="text-xl font-bold tracking-tight">{{ name }}</h3>
+          <h4 class="text-sm text-gray-600">{{ info }}</h4>
+        </div>
+
+        <div class="info-wrapper" :class="{ 'is-open pb-4': show }">
+          <div class="info text-base md:text-lg leading-snug tracking-tight text-justify">{{ content }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// ------------------------ Imports
+
+// -------- Components
+
+import Icon from "./Icon.vue";
+// -------- Vue
+
+import { ref } from "vue";
+
+// -------- Event Bus
+
+// -------- Store
+
+// -------- Router
+
+// -------- External
+
+// ------------------------ Props
+
+const props = defineProps({
+  image: String,
+  name: String,
+  info: String,
+  content: String,
+});
+
+// ------------------------ Variables
+
+let show = ref(false);
+
+// ------------------------ Computed
+
+// ------------------------ Functions
+
+// ------------------------ Events
+
+// -------- Lifecycle Hooks
+
+// -------- Event Bus
+
+// -------- Watch
+</script>
+
+<style scoped>
+.tile {
+  min-width: 300px;
+  max-width: 800px;
+}
+
+.image {
+  height: 300px;
+}
+
+.info-wrapper {
+  display: grid;
+  grid-template-rows: 0fr;
+  transition: grid-template-rows 0.5s ease-out;
+}
+
+.info-wrapper.is-open {
+  grid-template-rows: 1fr;
+}
+
+.info {
+  overflow: hidden;
+}
+
+.rotate-0 {
+  transform: rotate(0deg);
+  transition: all 0.5s ease;
+}
+
+.rotate-180 {
+  transform: rotate(180deg);
+  transition: all 0.5s ease;
+}
+
+.opacity-0 {
+  opacity: 0;
+  transition: all 0.5s ease;
+}
+
+.opacity-100 {
+  opacity: 100;
+  transition: all 0.5s ease;
+}
+</style>
