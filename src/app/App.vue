9586c9418bf2a247fc0138034d559141d6e529c9
David <davidg@controlconcepts.net>
2024-01-24 21:41:21 -0500

    Start redesigning layout / theme

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div id="app" class="w-full h-screen bg-gray-50 p-4 flex-col-standard justify-start">
-    <Header />
+  <div id="app" class="w-full h-full bg-custom-blue-dark p-4 flex-col text-custom-white font-inter-bold selection:text-custom-gray-dark selection:bg-custom-green-light">
+    <h1 class="mt-8 text-4xl font-bold tracking-tight">David Glassman</h1>
+
+    <div class="mt-4 w-full h-20 border-custom-white border flex-col-standard">Summary Section</div>
+    <div class="mt-8 w-full h-10 border-custom-white border flex-col-standard">Links/Socials Section</div>
+    <div class="about mt-28 w-full h-96 border-custom-white border flex-col-standard">About Section</div>
+    <div class="experience mt-16 w-full h-10 border-custom-white border flex-col-standard">Experience Section</div>
+    <div class="projects mt-16 w-full h-10 border-custom-white border flex-col-standard">Projects Section</div>
+    <div class="mt-16 w-full h-48 border-custom-white border flex-col-standard">Footer Section</div>
+    <!-- <Header />
     <About />
     <Projects />
     <Resume />
-    <Footer />
+    <Footer /> -->
   </div>
 </template>
 
@@ -13,11 +21,11 @@
 
 // -------- Components
 
-import Header from "../components/Header.vue";
+/* import Header from "../components/Header.vue";
 import About from "../components/About.vue";
 import Projects from "../components/Projects.vue";
 import Resume from "../components/Resume.vue";
-import Footer from "../components/Footer.vue";
+import Footer from "../components/Footer.vue"; */
 
 // -------- Vue
 
@@ -46,4 +54,16 @@ import Footer from "../components/Footer.vue";
 // -------- Watch
 </script>
 
-<style scoped></style>
+<style scoped>
+.about {
+  height: 35rem;
+}
+
+.experience {
+  height: 84rem;
+}
+
+.projects {
+  height: 84rem;
+}
+</style>
