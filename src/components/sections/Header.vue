0ff5606f031759bf39c163f541fae4de3c26fd2a
David <davidg@controlconcepts.net>
2024-02-02 12:04:21 -0500

    Implement Tagline component with scrolling text

--- src/components/sections/Header.vue
+++ src/components/sections/Header.vue
@@ -1,11 +1,8 @@
 <template>
-  <header class="mb-20 xl:mb-32">
+  <header class="mb-20 min-w-[360px] xl:mb-32">
     <div class="text-4xl font-extrabold md:text-5xl"><span class="text-primary-2">Hey!</span> I'm <GlitchText text="David" />.</div>
 
-    <div class="mt-4">
-      <div class="text-lg text-grey-300">I'm a <span class="font-semibold text-grey-100 underline decoration-accent-1 decoration-dashed underline-offset-4">software developer</span> who</div>
-      <div class="text-lg font-semibold underline decoration-accent-1 decoration-dashed underline-offset-4">something something something.</div>
-    </div>
+    <Tagline />
   </header>
 </template>
 
@@ -13,7 +10,9 @@
 // ------------------------ Imports
 
 // -------- Components
+
 import GlitchText from "../atomic/GlitchText.vue";
+import Tagline from "./Tagline.vue";
 
 // -------- Vue
 
