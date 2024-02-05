84dbf84787db8e0b0cb940b383de73122c24d74c
David <davidg@controlconcepts.net>
2024-02-05 13:47:05 -0500

    Remove test and revert back to normal code

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="mb-20 min-w-[360px] xl:mb-32">
-    <div class="text-4xl font-extrabold md:text-5xl"><span class="text-green-500">Hey!</span> I'm <GlitchText text="David" />.</div>
+    <div class="text-4xl font-extrabold md:text-5xl"><span class="text-primary-2">Hey!</span> I'm <GlitchText text="David" />.</div>
 
     <Tagline />
   </header>
