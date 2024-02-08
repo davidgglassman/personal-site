0a5f1280f31ff1da8ed23f63e35b4f89a09a25f8
David <davidg@controlconcepts.net>
2024-02-08 16:51:59 -0500

    Adjust mobile header styling

--- components/shared/Header.vue
+++ components/shared/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="mb-16 flex flex-wrap justify-between align-middle sm:mb-24 sm:flex-nowrap lg:mb-32">
-    <ul class="flex w-full flex-col gap-2 sm:flex-row md:gap-1 lg:gap-2">
+    <ul class="flex w-full flex-col sm:flex-row sm:gap-2 md:gap-1 lg:gap-2">
       <li v-for="(item, i) in items" :key="i">
-        <NuxtLink :to="item.path" class="flex h-10 w-full items-center justify-start gap-4 rounded-xl border border-grey-400 bg-grey-100 p-4 text-grey-900 dark:border-grey-700 dark:bg-grey-950 dark:text-grey-300 sm:h-6 sm:justify-center sm:gap-2 md:h-8 md:w-auto lg:h-10">
+        <NuxtLink :to="item.path" :class="{ 'rounded-t-xl': i === 0, 'rounded-b-xl': i === items.length - 1 }" class="flex h-10 w-full items-center justify-start gap-4 border border-grey-400 bg-grey-100 p-4 text-grey-900 dark:border-grey-700 dark:bg-grey-950 dark:text-grey-300 sm:h-6 sm:justify-center sm:gap-2 sm:rounded-xl md:h-8 md:w-auto lg:h-10">
           <Icon :name="item.icon" class="h-4 w-4" />
           <p class="text-xs font-semibold lg:text-sm">{{ item.name }}</p>
         </NuxtLink>
