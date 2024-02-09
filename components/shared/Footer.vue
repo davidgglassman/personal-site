2e0c916ace3c2881355ff3134622d8fb4089d991
David <davidg@controlconcepts.net>
2024-02-09 12:38:24 -0500

    Add some top/bottom margin to header/footer on mobile

--- components/shared/Footer.vue
+++ components/shared/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="flex-row-standard h-8 w-full gap-4 sm:gap-6 lg:gap-8">
+  <footer class="flex-row-standard mb-4 h-8 w-full gap-4 sm:gap-6 md:mb-0 lg:gap-8">
     <div class="flex-col-standard">
       <p class="w-18 mb-1 text-[0.6rem] font-medium text-grey-700 dark:text-grey-600 sm:text-xs lg:text-sm">Built With</p>
       <a href="https://nuxt.com/" target="_blank">
--- components/shared/Header.vue
+++ components/shared/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="mb-16 flex flex-wrap justify-between align-middle sm:mb-24 sm:flex-nowrap lg:mb-32">
+  <header class="mb-16 mt-4 flex flex-wrap justify-between align-middle sm:mb-24 sm:flex-nowrap md:mt-0 lg:mb-32">
     <ul class="flex w-full flex-col sm:flex-row sm:gap-2 md:gap-1 lg:gap-2">
       <li v-for="(item, i) in items" :key="i">
         <NuxtLink :to="item.path" :class="{ 'rounded-t-xl': i === 0, 'rounded-b-xl': i === items.length - 1 }" class="flex h-10 w-full items-center justify-start gap-4 border border-grey-400 bg-grey-100 p-4 text-grey-900 dark:border-grey-700 dark:bg-grey-950 dark:text-grey-300 sm:h-6 sm:justify-center sm:gap-2 sm:rounded-xl md:h-8 md:w-auto lg:h-10">
