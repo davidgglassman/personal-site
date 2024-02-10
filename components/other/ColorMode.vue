fa97b385ffa52f02dd2e892f202be10fd96b0607
David <davidg@controlconcepts.net>
2024-02-10 10:21:04 -0500

    Change to hamburger menu on mobile

--- components/other/ColorMode.vue
+++ components/other/ColorMode.vue
@@ -1,6 +1,6 @@
 <template>
   <ClientOnly>
-    <button class="flex-row-standard mx-auto mt-4 h-8 w-8 rounded-full border border-grey-400 bg-grey-100 opacity-75 dark:border-grey-700 dark:bg-grey-950 dark:text-grey-300 sm:mt-0 md:mx-0 lg:h-10 lg:w-10">
+    <button class="flex-row-standard h-8 w-8 rounded-full border border-grey-400 bg-grey-100 opacity-75 dark:border-grey-700 dark:bg-grey-950 dark:text-grey-300 sm:mt-0 md:mx-0 lg:h-10 lg:w-10">
       <Icon :name="'moon'" class="h-6 w-6 text-grey-800" :class="{ hidden: colorMode.value === 'dark' }" @click="colorMode.preference = 'dark'" />
       <Icon :name="'sun'" class="h-6 w-6 text-grey-100" :class="{ hidden: colorMode.value === 'light' }" @click="colorMode.preference = 'light'" />
     </button>
--- components/shared/Header.vue
+++ components/shared/Header.vue
@@ -1,8 +1,11 @@
 <template>
-  <header class="mb-16 mt-4 flex flex-wrap justify-between align-middle sm:mb-24 sm:flex-nowrap md:mt-0 lg:mb-32">
-    <ul class="flex w-full flex-col sm:flex-row sm:gap-2 md:gap-1 lg:gap-2">
+  <header class="mt-4 flex flex-wrap justify-between align-middle sm:mb-24 sm:flex-nowrap md:mt-0 lg:mb-32" :class="{ 'mb-16': !menuActive }">
+    <Icon name="menu" class="h-8 w-8 sm:hidden" :class="{ hidden: menuActive === true }" @click="menuActive = true" />
+    <Icon name="close" class="h-7 w-7 sm:hidden" :class="{ hidden: menuActive === false }" @click="menuActive = false" />
+
+    <ul class="hidden w-full gap-1 sm:flex lg:gap-2">
       <li v-for="(item, i) in filteredItems" :key="i">
-        <NuxtLink :to="item.path" :class="{ 'rounded-t-xl': i === 0, 'rounded-b-xl': i === items.length - 1 }" class="flex h-10 w-full items-center justify-start gap-4 border border-grey-400 bg-grey-100 p-4 text-grey-900 dark:border-grey-700 dark:bg-grey-950 dark:text-grey-300 sm:h-6 sm:justify-center sm:gap-2 sm:rounded-xl md:h-8 md:w-auto lg:h-10">
+        <NuxtLink :to="item.path" class="flex h-10 w-full items-center justify-start gap-4 border border-grey-400 bg-grey-100 p-4 text-grey-900 dark:border-grey-700 dark:bg-grey-950 dark:text-grey-300 sm:h-6 sm:justify-center sm:gap-2 sm:rounded-xl md:h-8 md:w-auto lg:h-10">
           <Icon :name="item.icon" class="h-4 w-4" />
           <p class="text-xs font-semibold lg:text-sm">{{ item.name }}</p>
         </NuxtLink>
@@ -11,6 +14,15 @@
 
     <ColorMode />
   </header>
+
+  <ul class="mb-8 mt-3 flex w-full flex-col border border-grey-400 dark:border-grey-700 sm:flex-row sm:gap-2 md:gap-1 lg:gap-2" :class="{ hidden: !menuActive }">
+    <li v-for="(item, i) in filteredItems" :key="i">
+      <NuxtLink :to="item.path" @click="menuActive = false" class="flex h-10 w-full items-center justify-start gap-4 border border-grey-400 bg-grey-100 p-4 text-grey-900 dark:border-grey-700 dark:bg-grey-950 dark:text-grey-300 sm:h-6 sm:justify-center sm:gap-2 sm:rounded-xl md:h-8 md:w-auto lg:h-10">
+        <Icon :name="item.icon" class="h-4 w-4" />
+        <p class="text-xs font-semibold lg:text-sm">{{ item.name }}</p>
+      </NuxtLink>
+    </li>
+  </ul>
 </template>
 
 <script setup>
@@ -27,6 +39,8 @@
 // ------------------------ Variables
 
 const { showBlog } = await useShowBlog();
+
+const menuActive = ref(false);
 
 const items = [
   {
@@ -67,11 +81,23 @@ const filteredItems = computed(() => {
 
 // -------- Lifecycle Hooks
 
+onMounted(() => {
+  const mediaQuery = window.matchMedia("(min-width: 440px)");
+
+  function handleMediaChange(e) {
+    if (e.matches) {
+      menuActive.value = false;
+    }
+  }
+
+  mediaQuery.addEventListener("change", handleMediaChange);
+});
+
 // -------- Watch
 </script>
 
 <style scoped>
 .router-link-exact-active {
-  @apply bg-grey-200 dark:bg-grey-700;
+  @apply bg-grey-200 dark:bg-grey-800;
 }
 </style>
