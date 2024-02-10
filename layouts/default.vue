048b2f93fa9659a9897a0a8c1e4a5a224928b048
David <davidg@controlconcepts.net>
2024-02-10 09:13:57 -0500

    Test to see if can change scroll-bounce background color on mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-full min-h-screen w-full min-w-[300px] cursor-default flex-col bg-grey-100 text-left font-inter text-sm font-normal text-grey-900 selection:bg-accent-1 selection:text-grey-900 dark:bg-grey-950 dark:text-grey-400 sm:text-base lg:text-lg">
+  <div class="canvas h-full min-h-screen w-full min-w-[300px] cursor-default flex-col bg-grey-100 text-left font-inter text-sm font-normal text-grey-900 selection:bg-accent-1 selection:text-grey-900 dark:bg-grey-950 dark:text-grey-400 sm:text-base lg:text-lg">
     <div class="mx-auto flex h-full min-h-screen w-full max-w-[49rem] flex-col justify-between p-4 md:p-8">
       <div class="flex h-auto flex-col justify-between">
         <Header />
