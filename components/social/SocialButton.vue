d12425ea68b1dc794975c2fa02a9e2c0c89d3edc
David Glassman <davidglassman@fastmail.com>
2025-01-18 17:38:02 -0500

    Add SocialCard

---
+++ components/social/SocialButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <a
+    :href="href"
+    target="_blank">
+    <button
+      class="flex size-12 items-center justify-center rounded-xl border border-gray-200 p-2 transition-colors hover:border-gray-300 dark:border-gray-800 dark:hover:border-gray-700">
+      <Icon
+        :name="iconName"
+        class="size-7 text-gray-400/70 dark:text-gray-400" />
+    </button>
+  </a>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  iconName: string;
+  href: string;
+}>();
+</script>
--- components/social/SocialCard.vue
+++ components/social/SocialCard.vue
@@ -1,7 +1,27 @@
 <template>
-  <CardTemplate> Social </CardTemplate>
+  <CardTemplate
+    :fixed="true"
+    :span="'span 4'">
+    <div class="flex h-full w-full items-center justify-center gap-3">
+      <SocialButton
+        icon-name="mdi:email-outline"
+        href="mailto:davidglassman@fastmail.com" />
+
+      <SocialButton
+        icon-name="mdi:linkedin"
+        href="https://linkedin.com/in/davidglassman29/" />
+
+      <SocialButton
+        icon-name="mdi:instagram"
+        href="https://instagram.com/davidgglassman/" />
+
+      <SocialButton
+        icon-name="mdi:github"
+        href="https://github.com/davidgglassman" />
+    </div>
+  </CardTemplate>
 </template>
 
-<script setup></script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
