4d3f90c692bd363fac9c9e61f151d2983031bc7b
David Glassman <davidglassman@fastmail.com>
2025-02-01 12:49:37 -0500

    Add Resume card

--- components/resume/ResumeCard.vue
+++ components/resume/ResumeCard.vue
@@ -1,10 +1,22 @@
 <template>
   <CardTemplate>
-    <template #header>
-      <h3>Resume</h3>
-    </template>
+    <template #content>
+      <div class="flex h-auto w-full items-center justify-between">
+        <h2 class="text-lg font-semibold text-gray-500 dark:text-gray-300">Resume</h2>
 
-    <template #content>TODO</template>
+        <div class="flex items-center justify-end gap-3">
+          <ResumeButton
+            icon-name="mdi:eye-outline"
+            href="resume.pdf"
+            target="_blank" />
+
+          <ResumeButton
+            icon-name="material-symbols:download-rounded"
+            href="resume.pdf"
+            download="david-glassman-resume-2025.pdf" />
+        </div>
+      </div>
+    </template>
   </CardTemplate>
 </template>
 
