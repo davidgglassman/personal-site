296bd4075da99c6bcc3872ab13cba6cdd1d0cede
David Glassman <davidglassman@fastmail.com>
2025-05-26 17:53:10 -0400

    Initial foundation code

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <p class="text-red-500">Hello World!</p>
+</template>
+
+<script setup lang="ts">
+// ------------------------ Imports
+
+// -------- Components
+
+// -------- Vue
+
+// -------- Internal
+
+// -------- External
+
+// -------- Event Bus
+
+// -------- Store
+
+// ------------------------ Props
+
+// ------------------------ Variables
+
+// ------------------------ Computed
+
+// ------------------------ Functions
+
+// ------------------------ Events
+
+// -------- Lifecycle Hooks
+
+// -------- Event Bus
+
+// -------- Watch
+</script>
+
+<style scoped></style>
