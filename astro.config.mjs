3955211089da739f607fd4353b2308fbbe303a26
David Glassman <davidglassman@fastmail.com>
2025-06-03 19:13:39 -0400

    Missed file in last commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     "/": "/under-construction",
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
