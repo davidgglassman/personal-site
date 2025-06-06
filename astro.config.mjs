ebcb9fcc9f3875e31e96b06b0950fdebca9c7f24
David Glassman <davidglassman@fastmail.com>
2025-06-06 19:29:34 -0400

    Add initial sections to Writing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
@@ -8,6 +7,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
