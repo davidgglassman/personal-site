e110274771e0bff6cf05b9ebe2c46949b7fdd8f4
David Glassman <davidglassman@fastmail.com>
2025-06-11 18:10:15 -0400

    Implement ThemeSelector

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
@@ -14,5 +16,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [icon()],
+  integrations: [icon(), react()],
 });
---
+++ src/components/shared/ThemeSelector.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
+
+const ThemeSelector = () => {
+  useEffect(() => {
+    const updateTheme = () => {
+      if (
+        localStorage.theme === "dark" ||
+        (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+      ) {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+    };
+
+    updateTheme();
+
+    window.addEventListener("astro:after-swap", updateTheme);
+
+    return () => {
+      window.removeEventListener("astro:after-swap", updateTheme);
+    };
+  }, []);
+
+  const toggleTheme = () => {
+    if (localStorage.theme === "dark") {
+      localStorage.theme = "light";
+      document.documentElement.classList.remove("dark");
+    } else {
+      localStorage.theme = "dark";
+      document.documentElement.classList.add("dark");
+    }
+  };
+
+  return (
+    <button type="button" id="theme-selector" onClick={toggleTheme} aria-label="Toggle theme">
+      <FaMoon className="hidden size-6 dark:inline" />
+      <FaSun className="inline size-6 dark:hidden" />
+    </button>
+  );
+};
+
+export default ThemeSelector;
