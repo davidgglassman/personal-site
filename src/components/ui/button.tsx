dd2959d2443002edb10faf8ac4c5760834c343ee
David Glassman <davidglassman@fastmail.com>
2025-06-13 14:54:06 -0400

    More general styling updates

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,8 +19,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-primary hover:text-[#a5a5a5]",
-        icon: "hover:text-[#a5a5a5]",
+        link: "text-primary hover:text-accent-foreground",
+        icon: "hover:text-accent-foreground",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
